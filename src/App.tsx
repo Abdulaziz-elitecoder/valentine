f911a5847c5090ed23fce7b5cda28b4388587e77
Abdulaziz Elnoti <abdulaziz.muhammed.elnoti@gmail.com>
2025-02-14 17:48:20 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -37,14 +37,14 @@ export default function Page() {
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <img src="will-you-be-my-valentine\src\assets\bear-kiss-bear-kisses.gif" />
           <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
         </>
       ) : (
         <>
           <img
             className="h-[200px]"
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+            src="will-you-be-my-valentine\src\assets\cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
           <h1 className="my-4 text-4xl">Atheer, Will you be my Valentine?</h1>
           <div className="flex items-center">
